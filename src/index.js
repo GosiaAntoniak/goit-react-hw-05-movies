69a91f62b24fee969d64345b83d727a454a6acf8
Gosia Antoniak/gosia-antoniak_89@o2.pl <gosia-antoniak_89@o2.pl>
2023-03-16 11:28:38 +0100

    feat: final commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"
+import "./index.css";
+import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
